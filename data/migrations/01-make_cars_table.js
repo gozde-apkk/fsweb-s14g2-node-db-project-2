f57750ba54d4aad44650e1dca37c708b84cdb565
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-06-25 18:59:24 +0300

    cars_table created-fixed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,36 @@
+
+
+  /**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+
+
 exports.up = function (knex) {
   // SİHRİNİZİ GÖSTERİN
+  return knex.shema.createTable("Cars" , tbl=>{
+    tbl.increments();
+    tbl.string("vin")
+       .notNullable()
+       .unique();
+
+    tbl.string("make")
+       .notNullable();
+    tbl.string("model")
+       .notNullable();
+    tbl.integer()
+      .notNullable();
+    tbl.string("title");
+    tbl.string("transmission");
+    
+
+  })
+
 };
 
 exports.down = function (knex) {
   // SİHRİNİZİ GÖSTERİN
+  //yok etmek
+  return knex.schema.dropTableIfExists('Cars')
 };
